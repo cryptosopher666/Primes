2729ab68b78bf35f1711f216126d05cc2bf97ce6
Mohammad <m.mirgolbabaei666@gmail.com>
2023-05-04 09:30:33 +0330

    Update Primes.cpp

--- Primes.cpp
+++ Primes.cpp
@@ -1,30 +1,17 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
+#include<conio.h>
 using namespace std;
-
-int main() {
-
-    int a,i,j;
-
-    cout<<"Find prime numbers upto : ";
-    cin>>a;
-
-    cout<<endl<<"All prime numbers upto "<<a<<" are : "<<endl;
-
-    for(i=1;i<=a;i++) {
-
-        for(j=2;j<=(i/2);j++) {
-
-            if(i%j==0) {
-                j=i;
-                break;
-            }
-        }
-
-        if(j != i) {
-            cout<<i<<endl;
-        }
-    }
-
-    getch();
+main(){
+	int a,b=0;
+	cout<<"Enter number: ";
+	cin>>a;
+	for(int i=2;i<a;i++){
+		if(a%i==0)
+		b++;
+	}
+	if(b>0 || a==1)
+	cout<<"not prime";
+	else
+	cout<<"prime";
+	getch();
 }
